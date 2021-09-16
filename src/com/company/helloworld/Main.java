2d8a1e94a367d2f04e09f64f30ff3498b4578e55
sureshmcamv <sureshmcamv@gmail.com>
2021-09-16 20:16:40 +0530

    "testing..."

---
+++ src/com/company/helloworld/Main.java
@@ -0,0 +1,10 @@
+package com.company.helloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Testing the world.....");
+    }
+}
